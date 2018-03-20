52dfee877bf32f2a87d0b58a9c4dae5d5d26ac8f
FangWolf <siyu2510@163.com>
2018-03-20 21:19:00 +0800

    3缓冲区(异步订阅)，当缓冲区<=32且已处理>=96,则request(96)

--- app/src/main/java/com/fangwolf/test_rxjava/MainActivity.java
+++ app/src/main/java/com/fangwolf/test_rxjava/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     Button fire5_1;
     Button fire6;
     Button fire7;
-    Button fire8;
+    Button fire7_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
         fire5 = findViewById(R.id.fire5);
         fire5_1 = findViewById(R.id.fire5_1);
         fire6 = findViewById(R.id.fire6);
+        fire7 = findViewById(R.id.fire7);
+        fire7_1 = findViewById(R.id.fire7_1);
 
         fire.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -389,5 +391,70 @@ public class MainActivity extends AppCompatActivity {
                         });
             }
         });
+        fire7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 被观察者：一共需要发送500个事件，但真正开始发送事件的前提 = FlowableEmitter.requested()返回值 ≠ 0
+                // 观察者：每次接收事件数量 = 48（点击按钮）
+                Flowable.create(new FlowableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+
+                        Log.d(TAG, "观察者可接收事件数量 = " + emitter.requested());
+                        boolean flag; //设置标记位控制
+
+                        // 被观察者一共需要发送500个事件
+                        for (int i = 0; i < 500; i++) {
+                            flag = false;
+
+                            // 若requested() == 0则不发送
+                            while (emitter.requested() == 0) {
+                                if (!flag) {
+                                    Log.d(TAG, "不再发送");
+                                    flag = true;
+                                }
+                            }
+                            // requested() ≠ 0 才发送
+                            Log.d(TAG, "发送了事件" + i + "，观察者可接收事件数量 = " + emitter.requested());
+                            emitter.onNext(i);
+
+
+                        }
+                    }
+                }, BackpressureStrategy.ERROR).subscribeOn(Schedulers.io()) // 设置被观察者在io线程中进行
+                        .observeOn(AndroidSchedulers.mainThread()) // 设置观察者在主线程中进行
+                        .subscribe(new Subscriber<Integer>() {
+                            @Override
+                            public void onSubscribe(Subscription s) {
+                                Log.d(TAG, "onSubscribe");
+                                mSubscription = s;
+                                // 初始状态 = 不接收事件；通过点击按钮接收事件
+                            }
+
+                            @Override
+                            public void onNext(Integer integer) {
+                                Log.d(TAG, "接收到了事件" + integer);
+                            }
+
+                            @Override
+                            public void onError(Throwable t) {
+                                Log.w(TAG, "onError: ", t);
+                            }
+
+                            @Override
+                            public void onComplete() {
+                                Log.d(TAG, "onComplete");
+                            }
+                        });
+            }
+        });
+        fire7_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSubscription.request(48);
+                // 点击按钮 则 接收48个事件
+            }
+        });
+
     }
 }
