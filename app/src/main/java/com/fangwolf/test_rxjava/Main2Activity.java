2b84152110d73a4cd712b91f10d360161331e9dd
FangWolf <siyu2510@163.com>
2018-03-21 16:39:21 +0800

    concat/concatArray 合并观察者 按顺序发送

--- app/src/main/java/com/fangwolf/test_rxjava/Main2Activity.java
+++ app/src/main/java/com/fangwolf/test_rxjava/Main2Activity.java
@@ -14,6 +14,7 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
 
     String TAG = "Main2Activity";
     Button btn0;
+    Button btn0_0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +22,48 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_main2);
 
         findViewById(R.id.btn0).setOnClickListener(this);
+        findViewById(R.id.btn0_0).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn0:
+                // concat（）：组合多个被观察者（≤4个）一起发送数据
                 Observable.concat(Observable.just(1, 2, 3),
                         Observable.just(4, 5, 6),
                         Observable.just(7, 8, 9),
                         Observable.just(10, 11, 12))
+                        .subscribe(new Observer<Integer>() {
+                            @Override
+                            public void onSubscribe(Disposable d) {
+
+                            }
+
+                            @Override
+                            public void onNext(Integer value) {
+                                Log.d(TAG, "接收到了事件" + value);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                Log.d(TAG, "对Error事件作出响应");
+                            }
+
+                            @Override
+                            public void onComplete() {
+                                Log.d(TAG, "对Complete事件作出响应");
+                            }
+                        });
+                break;
+            case R.id.btn0_0:
+                // concatArray（）：组合多个被观察者一起发送数据（可＞4个）
+                // 注：串行执行
+                Observable.concatArray(Observable.just(1, 2, 3),
+                        Observable.just(4, 5, 6),
+                        Observable.just(7, 8, 9),
+                        Observable.just(10, 11, 12),
+                        Observable.just(13, 14, 15))
                         .subscribe(new Observer<Integer>() {
                             @Override
                             public void onSubscribe(Disposable d) {
