1be1a320c2425aeb0b646c12cbdcb2265d79aef9
FangWolf <siyu2510@163.com>
2018-03-21 17:05:44 +0800

    concatDelayError（） / mergeDelayError（） 发送完后再处理error事件

--- app/src/main/java/com/fangwolf/test_rxjava/Main2Activity.java
+++ app/src/main/java/com/fangwolf/test_rxjava/Main2Activity.java
@@ -6,15 +6,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
 
     String TAG = "Main2Activity";
-    Button btn0;
-    Button btn0_0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,10 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
 
         findViewById(R.id.btn0).setOnClickListener(this);
         findViewById(R.id.btn0_0).setOnClickListener(this);
+        findViewById(R.id.btn1).setOnClickListener(this);
+        findViewById(R.id.btn1_0).setOnClickListener(this);
+        findViewById(R.id.btn2).setOnClickListener(this);
+        findViewById(R.id.btn2_0).setOnClickListener(this);
     }
 
     @Override
@@ -64,6 +70,106 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
                         Observable.just(7, 8, 9),
                         Observable.just(10, 11, 12),
                         Observable.just(13, 14, 15))
+                        .subscribe(new Observer<Integer>() {
+                            @Override
+                            public void onSubscribe(Disposable d) {
+
+                            }
+
+                            @Override
+                            public void onNext(Integer value) {
+                                Log.d(TAG, "接收到了事件" + value);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                Log.d(TAG, "对Error事件作出响应");
+                            }
+
+                            @Override
+                            public void onComplete() {
+                                Log.d(TAG, "对Complete事件作出响应");
+                            }
+                        });
+                break;
+            case R.id.btn1:
+                // merge（）：组合多个被观察者（＜4个）一起发送数据
+                // 注：合并后按照时间线并行执行
+                Observable.merge(
+                        Observable.intervalRange(0, 3, 1, 1, TimeUnit.SECONDS), // 从0开始发送、共发送3个数据、第1次事件延迟发送时间 = 1s、间隔时间 = 1s
+                        Observable.intervalRange(2, 3, 1, 1, TimeUnit.SECONDS)) // 从2开始发送、共发送3个数据、第1次事件延迟发送时间 = 1s、间隔时间 = 1s
+                        .subscribe(new Observer<Long>() {
+                            @Override
+                            public void onSubscribe(Disposable d) {
+
+                            }
+
+                            @Override
+                            public void onNext(Long value) {
+                                Log.d(TAG, "接收到了事件" + value);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                Log.d(TAG, "对Error事件作出响应");
+                            }
+
+                            @Override
+                            public void onComplete() {
+                                Log.d(TAG, "对Complete事件作出响应");
+                            }
+                        });
+                break;
+            // mergeArray（） = 组合4个以上的被观察者一起发送数据
+            case R.id.btn2:
+                Observable.concat(
+                        Observable.create(new ObservableOnSubscribe<Integer>() {
+                            @Override
+                            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                                emitter.onNext(1);
+                                emitter.onNext(2);
+                                emitter.onNext(3);
+                                emitter.onError(new NullPointerException()); // 发送Error事件，因为无使用concatDelayError，所以第2个Observable将不会发送事件
+                                emitter.onComplete();
+                            }
+                        }),
+                        Observable.just(4, 5, 6))
+                        .subscribe(new Observer<Integer>() {
+                            @Override
+                            public void onSubscribe(Disposable d) {
+
+                            }
+
+                            @Override
+                            public void onNext(Integer value) {
+                                Log.d(TAG, "接收到了事件" + value);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                Log.d(TAG, "对Error事件作出响应");
+                            }
+
+                            @Override
+                            public void onComplete() {
+                                Log.d(TAG, "对Complete事件作出响应");
+                            }
+                        });
+                break;
+            case R.id.btn2_0:
+                Observable.concatArrayDelayError(
+                        Observable.create(new ObservableOnSubscribe<Integer>() {
+                            @Override
+                            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+
+                                emitter.onNext(1);
+                                emitter.onNext(2);
+                                emitter.onNext(3);
+                                emitter.onError(new NullPointerException()); // 发送Error事件，因为使用了concatDelayError，所以第2个Observable将会发送事件，等发送完毕后，再发送错误事件
+                                emitter.onComplete();
+                            }
+                        }),
+                        Observable.just(4, 5, 6))
                         .subscribe(new Observer<Integer>() {
                             @Override
                             public void onSubscribe(Disposable d) {
