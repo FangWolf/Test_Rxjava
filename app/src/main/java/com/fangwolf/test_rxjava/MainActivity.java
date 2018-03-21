a855b0b72c8ee04ca14295ace21e86249d3ec2b3
FangWolf <siyu2510@163.com>
2018-03-21 15:57:51 +0800

    map变换符

--- app/src/main/java/com/fangwolf/test_rxjava/MainActivity.java
+++ app/src/main/java/com/fangwolf/test_rxjava/MainActivity.java
@@ -22,6 +22,8 @@ import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -497,6 +499,32 @@ public class MainActivity extends AppCompatActivity {
                                 Log.d(TAG, "onComplete");
                             }
                         });
+            }
+        });
+
+        // 采用RxJava基于事件流的链式操作
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+
+            // 1. 被观察者发送事件 = 参数为整型 = 1、2、3
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+
+            }
+            // 2. 使用Map变换操作符中的Function函数对被观察者发送的事件进行统一变换：整型变换成字符串类型
+        }).map(new Function<Integer, String>() {
+            @Override
+            public String apply(Integer integer) throws Exception {
+                return "使用 Map变换操作符 将事件" + integer +"的参数从 整型"+integer + " 变换成 字符串类型" + integer ;
+            }
+        }).subscribe(new Consumer<String>() {
+
+            // 3. 观察者接收事件时，是接收到变换后的事件 = 字符串类型
+            @Override
+            public void accept(String s) throws Exception {
+                Log.d(TAG, s);
             }
         });
     }
